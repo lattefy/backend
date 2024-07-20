690d1ff9a751d09766be324c7db56ece297f3136
Lattefy <lattefy.ai@gmail.com>
2024-07-20 20:22:51 -0300

    Add files via upload

---
+++ models/Client.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const ClientSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  birthDate: { type: String, required: true },
+  lastRating: { type: Number, required: true },
+  averageRating: { type: Number, required: true },
+  discountAvailable: { type: Boolean, required: true },
+  emissionDate: { type: Date, required: true },
+  expirationDate: { type: Date, required: true },
+  discountsGotten: { type: Number, required: true },
+  discountsClaimed: { type: Number, required: true },
+  totalSpent: { type: Number, required: true },
+  averageExpenditure: { type: Number, required: true }
+});
+
+module.exports = mongoose.model("Client", ClientSchema);
