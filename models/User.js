48588072e1fae5db5fe4350c9b1aa4539da55a68
Lattefy <lattefy.ai@gmail.com>
2024-09-30 20:05:29 -0300

    user - password

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
   business: { type: String, required: true },
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   phoneNumber: { type: String, required: true },
   accountType: { type: Boolean, required: true },
   dashboardLink: { type: String, required: true }
